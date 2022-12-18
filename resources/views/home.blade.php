d28dd7d42e3890dad7d197f3e85fc09c1fb193b7
chernanat <chernan1234@hotmail.com>
2022-12-18 15:30:49 -0500

    updating chages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <section>
+        <main>
+            <home-index></home-index>
+        </main>
+    </section>
+</x-app-layout>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <section>
+        <main>
+            <login-user></login-user>
+        </main>
+    </section>
+</x-app-layout>
